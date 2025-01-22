7a23d4baf57f661ac6387e2868730d07ddf36ac8
Joao Victor <correia.jv16@gmail.com>
2025-01-22 00:27:06 -0300

    add page chamados
    Co-authored-by: Mateus Caltabiano <mateus.frauzino@gmail.com>
    Co-authored-by: Matheus Soares <matheus.1503@hotmail.com>

---
+++ src/app/(auth)/chamados/page.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '@/store/store'
+import { fetchTickets } from '@/store/slices/ticketSlice'
+import { toast } from 'react-toastify'
+import { TicketDTO } from '@/types/Tickets'
+import TicketModal from '@/components/chamados/TicketModal'
+
+
+export default function Suporte() {
+  const dispatch = useDispatch<AppDispatch>()
+  const { tickets, loading, error } = useSelector(
+    (state: RootState) => state.ticket
+  )
+  const [selectedTicket, setSelectedTicket] = useState <TicketDTO> ();
+  const [isModalOpen, setIsModalOpen] = useState (false);
+
+  const session = useSession()
+
+  const handleCardClick = async (ticket : TicketDTO) => {
+      setSelectedTicket(ticket);
+      setIsModalOpen(true);
+    }
+
+  useEffect(() => {
+    dispatch(fetchTickets())
+  }, [dispatch, session.data?.user.id])
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error)
+    }
+  }, [error])
+
+  if (session.data?.user.role !== 'ADMIN') {
+    return (
+      <div className="mx-6 rounded-lg bg-white p-6 shadow-lg">
+        <div className="mb-4 flex items-center justify-between">
+          Você não possui autorização para visualizar essa tela!
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="mx-6 rounded-lg bg-white p-6 shadow-lg">
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-lg font-semibold">Suporte</h2>
+      </div>
+
+      {isModalOpen && selectedTicket && (
+          <TicketModal
+            ticket={selectedTicket}
+            isModalOpen={isModalOpen}
+            onClose={() => {
+              setIsModalOpen(false);
+              setSelectedTicket(undefined); 
+              dispatch(fetchTickets()); 
+            }}
+          />
+        )}
+      {loading && <p>Carregando tickets...</p>}
+
+      {!loading && tickets.length === 0 && <p>Nenhum ticket encontrado.</p>}
+
+      {!loading && tickets.length > 0 && (
+        <ul className="space-y-4">
+          {tickets.map((ticket) => (
+           <li
+           onClick={ () => handleCardClick(ticket)}
+           key={ticket.id}
+           className="rounded-lg border border-gray-300 p-6 shadow-md hover:shadow-lg transition-shadow hover:border-green-400 active:bg-gray-200 active:scale-95 cursor-pointer"
+         >
+           <div className="flex items-center justify-between mb-4">
+             <h3 className="text-lg font-semibold ">{ticket.title}</h3>
+             <span
+               className={`inline-block rounded-full px-3 py-1 text-sm font-semibold text-white ${
+                 ticket.status === 'OPEN' ? 'bg-green-500' : 'bg-red-500'
+               }`}
+             >
+               {ticket.status === 'OPEN' ? 'Aberto' : 'Fechado'}
+             </span>
+           </div>
+         
+           <p
+                className="line-clamp-2 text-gray-600 text-sm mb-4 break-words overflow-hidden text-ellipsis"
+                style={{ wordWrap: 'break-word', wordBreak: 'break-word', whiteSpace: 'pre-wrap' }}
+              >
+                {ticket.message}
+            </p>
+         
+           <div className="text-sm  space-y-1">
+             <div>
+               <span className="font-bold">Data:</span>{' '}
+               {new Date(ticket.createdAt).toLocaleDateString('pt-BR')}
+             </div>
+             <div>
+               <span className="font-bold">Criado por:</span> {ticket.directorName}
+             </div>
+           </div>
+         </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
